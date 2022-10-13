166cd5e609c2e30610690a8ccaeedfb4d4f31330
hardipgajera <hardipgajera.omnisttechhub.@gmailcom>
2022-10-13 21:22:36 +0530

    Initial Commit

---
+++ architecture-demo/architecture_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  architecture_demoApp.swift
+//  architecture-demo
+//
+//  Created by macbook on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct architecture_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
